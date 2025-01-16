16d3d1de57b4b7b990f2227a4b407fd6eafd24b2
Vlad Sokhatskyi <193589912+Sokhatskyi@users.noreply.github.com>
2025-01-16 13:47:19 +0200

    Update env.py

--- infrastructure/database/migrations/env.py
+++ infrastructure/database/migrations/env.py
@@ -7,6 +7,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from infrastructure.database.models import Base
+from tgbot.config import load_config
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,12 +23,21 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+main_config = load_config(".env")
+sqlalchemy_url = main_config.db.construct_sqlalchemy_url()
+
+config.set_main_option(
+    "sqlalchemy.url",
+    sqlalchemy_url.render_as_string(hide_password=False)
+)
 
 
 def run_migrations_offline() -> None:
