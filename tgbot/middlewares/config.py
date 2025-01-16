41189db357c9ffeeb5003af04c758c89c904e57c
Vlad Sokhatskyi <193589912+Sokhatskyi@users.noreply.github.com>
2025-01-16 13:00:37 +0200

    Added middlewares

---
+++ tgbot/middlewares/config.py
@@ -0,0 +1,20 @@
+from typing import Callable, Dict, Any, Awaitable
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+
+from tgbot.config import Config
+
+
+class ConfigMiddleware(BaseMiddleware):
+    def __init__(self, config: Config) -> None:
+        self.config = config
+
+    async def __call__(
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: Dict[str, Any]
+    ) -> Any:
+        data['config'] = self.config
+        return await handler(event, data)
